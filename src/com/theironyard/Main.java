8e8d110d0b850b94bf5a4b2ac598b06c23f7f463
Krista Blachian <KristaBlachian@outlook.com>
2018-04-12 15:18:18 -0400

    experimenting with sounds using command line args

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,10 +3,19 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-        MusicTest1 mt = new MusicTest1();
-        mt.play();
+//        MusicTest1 mt = new MusicTest1();
+//        mt.play();
+//
+//        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+//        mini.play();
 
-        MiniMiniMusicApp mini = new MiniMiniMusicApp();
-        mini.play();
+        MiniMusicCmdLine mini = new MiniMusicCmdLine();
+        if (args.length < 2) {
+            System.out.println("Dont forget the instrument and note args");
+        } else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            mini.play(instrument, note);
+        }
     }
 }
